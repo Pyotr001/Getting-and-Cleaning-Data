5117551d9ce70013c7f9cdb47c567ec6997fcf19
Pyotr001 <pyotr001@gmail.com>
2015-08-12 13:51:57 +0300

    12 августа
    перед сохранением изменений

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,30 @@
 # You should create one R script called run_analysis.R that does the following. 
+
 # Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
-# Uses descriptive activity names to name the activities in the data set
+data <- rbind((read.table("UCI HAR Dataset/test/X_test.txt")), 
+              (read.table("UCI HAR Dataset/train/X_train.txt")))
+
 # Appropriately labels the data set with descriptive variable names. 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+dataNames <- read.table("UCI HAR Dataset/features.txt")
+names(data) <- make.names(dataNames[[2]], unique = TRUE)
+
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+meanCol <- grep("mean\\(\\)", dataNames[[2]])
+stdCol <- grep("std\\(\\)", dataNames[[2]])
+Tidydata <- data[c(meanCol, stdCol)]
+
+# Uses descriptive activity names to name the activities in the data set
+
+subject <- rbind(read.table("UCI HAR Dataset/test/subject_test.txt"),
+                 read.table("UCI HAR Dataset/train/subject_train.txt"))
+names(subject) <- "subject"
+
+activity <-  rbind(read.table("UCI HAR Dataset/test/subject_test.txt"),
+                   read.table("UCI HAR Dataset/train/subject_train.txt"))
+names(activity) <- "activity"
+
+data <- cbind(subject, activity, data)
+# From the data set in step 4, creates a second, independent tidy data set with 
+        # the average of each variable for each activity and each subject.
 
---
+++ trainScript.R
@@ -0,0 +1,40 @@
+dataXtest <- read.csv("UCI HAR Dataset/test/X_test.txt", sep = "")
+datafwf <- read.fwf("UCI HAR Dataset/test/X_test.txt") 
+datatable<- read.table("UCI HAR Dataset/test/X_test.txt") 
+#csv предназначено для запятых в разделителях, а CSV2 для запятых в числах,
+# где раздетель двоеточие
+
+dataY <- read.csv("UCI HAR Dataset/test/Y_test.txt")
+
+#Работет правильно:
+dataXtable <- read.table("UCI HAR Dataset/test/X_test.txt")
+dataXtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
+
+# попробую идти по порядку, затем поменяю логику.
+# соединение таблиц.
+data <- rbind(dataXtest, dataXtrain)
+
+grep("mean", dataNames[[2]])
+dataNames[2]
+testVector <- c("a", "ab", "c")
+grep("a", testVector)
+grepl("a", testVector)
+
+meanCol <- grep("mean\\(\\)", dataNames[[2]])
+stdCol <- grep("std\\(\\)", dataNames[[2]])
+Tidydata <- data[c(meanCol, stdCol)]
+
+# для переименовывания
+names(data) <- dataNames[[2]]
+names(data) <- make.names(dataNames[[2]], unique = TRUE)
+make.names(names, unique = FALSE, allow_ = TRUE)
+dataNames <- cbind(dataNames, make.names(dataNames[[2]], unique = TRUE))
+
+# дабавить перменные 
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subject <- c(subjectTest, subjectTrain)
+
+activityTest <-  read.table("UCI HAR Dataset/test/subject_test.txt")
+activityTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
+names(activityTest) <- "activity"
